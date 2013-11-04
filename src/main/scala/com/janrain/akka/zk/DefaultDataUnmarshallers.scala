a61384649bc1711096f65f357d781707cdb5e83e
Kender Elford <kender@janrain.com>
2013-11-04 11:01:19 -0800

    add support for xml in the default unmarshallers

--- src/main/scala/com/janrain/akka/zk/DefaultDataUnmarshallers.scala
+++ src/main/scala/com/janrain/akka/zk/DefaultDataUnmarshallers.scala
@@ -1,9 +1,15 @@
 package com.janrain.akka.zk
 
+import scala.xml.{XML, Elem}
+
 trait DefaultDataUnmarshallers {
   import ZkConfigExtension.ValueUnmarshaller
   implicit object StringValueUnmarshaller extends ValueUnmarshaller[String] {
     def unmarshal(data: Array[Byte]): String = new String(data)
+  }
+
+  implicit object XmlValueUnmarshaller extends ValueUnmarshaller[Elem] {
+    def unmarshal(data: Array[Byte]): Elem = XML.loadString(new String(data))
   }
 }
 
